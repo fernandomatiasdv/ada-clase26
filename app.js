a129f51f7ae97de040fbf2947aaefa4c9b5f915a
Fernando del Valle <fernandomatiasdv@gmail.com>
2021-02-05 20:55:49 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+    Es una nota en bloque
+
+
+//'use strict'; // Es un comentario en linea
+var nombre = prompt("Decime tu nombre");
+var apellido = prompt("Decime tu apellido");
+
+var nombre = prompt("Decime tu nombre");
+var apellido = prompt("Decime tu apellido");
+
+console.log("Tu nombre es " + nombre + " " + apellido)   
+console.log(`Tu nombre es ${nombre} ${apellido}`);
+
+console.log(numero1 + numero2);
+console.log(numero1 - numero2);
+console.log(numero1 / numero2);
+console.log(numero1 * numero2);
+
+*/
+var numero1 = 9,
+    numero2 = 3;
+
+console.log(numero1 * numero2);
+
+
+
+
+
+
+
