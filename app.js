2ffab447b2dc77df9325ce07f742db3234aeffad
Fernando del Valle <fernandomatiasdv@gmail.com>
2021-02-05 21:49:23 -0300

    actualizacion

--- app.js
+++ app.js
@@ -17,11 +17,35 @@ console.log(numero1 - numero2);
 console.log(numero1 / numero2);
 console.log(numero1 * numero2);
 
-*/
-var numero1 = 9,
-    numero2 = 3;
+// El typeof marca el tipo de dato:
+//    string
+//    number
+//    date
+//    boolean
 
-console.log(numero1 * numero2);
+
+console.log(typeof multiplicacion);
+console.log(typeof "Hola");
+console.log(typeof false);
+
+var numero1 = "Hola", //prompt("Ingresa un numero"),
+    numero2 = 1;
+const suma = Number(numero1) + numero2;
+console.log(`El resultado es ${suma}`);
+console.log(typeof numero1);
+console.log(typeof numero2);
+console.log("El resultado es " + numero1);
+*/
+
+
+let numero1 = prompt("Numero 1:"),
+    numero2 = prompt("Numero 2:");
+
+let resultado = Number(numero1) + Number(numero2);
+let resultado2 = numero1 + numero2;
+
+console.log(`El resultado es ${resultado}`);
+console.log(`El resultado es ${resultado2}`);
 
 
 
